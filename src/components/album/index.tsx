e029c99b4e92dc594c4bf1d8ab710983223fe397
Maires Souza <trabalho.maires@gmail.com>
2023-08-28 18:47:41 -0300

    feat: create music list of selected album

--- src/components/album/index.tsx
+++ src/components/album/index.tsx
@@ -1,6 +1,44 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import getMusics from '../../services/musicsAPI';
+import { AlbumType, SongType } from '../../types';
+import Loading from '../loading';
+import MusicCard from '../musicCard';
+import './Album.css';
+
 function Album() {
+  const { id } = useParams<{ id: string }>();
+  const [loading, setLoading] = useState(false);
+  const [album, setAlbum] = useState<AlbumType | null>(null);
+  const [songs, setSongs] = useState<SongType[]>([]);
+
+  useEffect(() => {
+    async function albumMusics() {
+      setLoading(true);
+      if (id) {
+        const [albumData, ...albunSongs] = await getMusics(id);
+        setAlbum(albumData);
+        setSongs(albunSongs);
+        setLoading(false);
+      }
+    }
+    albumMusics();
+  }, [id]);
+
   return (
-    <h1>Page Album</h1>
+    <div className="album-container">
+      {loading && <Loading />}
+      <img className="album-image" src={ album?.artworkUrl100 } alt="" />
+      <p className="artist-name" data-testid="artist-name">{album?.artistName}</p>
+      <p className="album-name" data-testid="album-name">{album?.collectionName}</p>
+      {songs.map((song) => (
+        <MusicCard
+          key={ song.trackId }
+          trackName={ song.trackName }
+          previewUrl={ song.previewUrl }
+        />
+      ))}
+    </div>
   );
 }
 
---
+++ src/components/musicCard/index.tsx
@@ -0,0 +1,24 @@
+import './MusicCard.css';
+
+type MusicCardProps = {
+  trackName: string,
+  previewUrl: string,
+};
+
+function MusicCard({ trackName, previewUrl }: MusicCardProps) {
+  return (
+    <div className="music-card-container">
+      <h2>{trackName}</h2>
+      <audio data-testid="audio-component" src={ previewUrl } controls>
+        <track kind="captions" />
+        O seu navegador não suporta o elemento
+        {' '}
+        {' '}
+        <code>audio</code>
+        .
+      </audio>
+    </div>
+  );
+}
+
+export default MusicCard;
