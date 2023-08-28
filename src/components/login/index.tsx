d7fd9cc89c04336dc99a267c7e9a5a679031f964
Maires Souza <trabalho.maires@gmail.com>
2023-08-28 10:08:53 -0300

    feat: Login form implemented

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,6 +1,59 @@
+import { useEffect, useState } from 'react';
+import './Login.css';
+import { useNavigate } from 'react-router-dom';
+import { createUser } from '../../services/userAPI';
+import Loading from '../loading';
+
 function Login() {
+  const [inputName, setInputName] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function handeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+    setInputName(value);
+  }
+
+  function handleClick() {
+    setLoading(true);
+  }
+
+  useEffect(() => {
+    async function newUSer() {
+      await createUser({ name: inputName });
+      navigate('/Search');
+    }
+    if (loading) {
+      newUSer();
+    }
+  }, [inputName, loading, navigate]);
+
+  if (loading) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
-    <h1>Page Login</h1>
+    <div className="form-container">
+      <form action="" method="get" encType="multipart/form-data">
+        <img src="./assets/logo.svg" alt="Logo" />
+        <input
+          data-testid="login-name-input"
+          type="text"
+          onChange={ handeChange }
+          value={ inputName }
+          placeholder="Qual é o seu nome?"
+        />
+        <button
+          disabled={ inputName.length < 3 }
+          data-testid="login-submit-button"
+          onClick={ handleClick }
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
   );
 }
 
