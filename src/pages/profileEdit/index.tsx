67f1db44651381e09a599b969c58d54f835f7ff6
Maires Souza <trabalho.maires@gmail.com>
2023-09-01 21:15:16 -0300

    feat: form to edit profile implemented

--- src/pages/profileEdit/index.tsx
+++ src/pages/profileEdit/index.tsx
@@ -1,6 +1,99 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getUser, updateUser } from '../../services/userAPI';
+import { UserType } from '../../types';
+import Loading from '../loading';
+
 function ProfileEdit() {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [user, setUser] = useState<UserType>({
+    name: '',
+    email: '',
+    description: '',
+    image: '',
+  });
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchUser = async () => {
+      const newUser = await getUser();
+      setUser(newUser);
+      setLoading(false);
+    };
+    fetchUser();
+  }, []);
+
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+  const buttonValidation = user.name.trim().length > 0
+  && user.email.trim().length > 0
+  && user.description.trim().length > 0
+  && emailRegex.test(user.email);
+
+  function handleSubmit() {
+    setLoading(true);
+    updateUser(user);
+    navigate('/profile');
+    setLoading(false);
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
+  if (loading) return <Loading />;
+
   return (
-    <h1>Page ProfileEdit</h1>
+    <form action="">
+      <label htmlFor="edit-name">Editar nome:</label>
+      <input
+        id="edit-name"
+        name="name"
+        data-testid="edit-input-name"
+        type="text"
+        onChange={ handleChange }
+        value={ user.name }
+      />
+
+      <label htmlFor="edit-email">Editar email:</label>
+      <input
+        id="edit-email"
+        name="email"
+        data-testid="edit-input-email"
+        type="email"
+        onChange={ handleChange }
+        value={ user.email }
+      />
+
+      <label htmlFor="edit-description">Editar descrição:</label>
+      <input
+        id="edit-description"
+        name="description"
+        data-testid="edit-input-description"
+        onChange={ handleChange }
+        value={ user.description }
+      />
+
+      <label htmlFor="edit-image">Alterar imagem:</label>
+      <input
+        id="edit-image"
+        name="image"
+        data-testid="edit-input-image"
+        value={ user.image }
+        onChange={ handleChange }
+      />
+
+      <button
+        type="submit"
+        data-testid="edit-button-save"
+        disabled={ !buttonValidation }
+        onClick={ handleSubmit }
+      >
+        Salvar
+      </button>
+    </form>
   );
 }
 
